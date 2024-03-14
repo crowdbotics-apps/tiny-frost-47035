7c404de281a34620061358a6af5d14731db40c20
Crowdbotics <team@crowdbotics.com>
2024-03-14 05:38:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-frost-47035",
+  url: "https://tiny-frost-47035.botics.co",
+  api: "https://tiny-frost-47035.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
